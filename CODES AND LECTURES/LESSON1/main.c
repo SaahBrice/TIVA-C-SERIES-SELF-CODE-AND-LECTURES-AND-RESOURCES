23550568ee12ca4d3bf3090ff37ff29fa1b567b2
SaahBrice <saahbrice98@gmail.com>
2025-05-02 12:29:00 +0200

    Addition of comments
    Comments addition to make code comprehensible

--- CODES AND LECTURES/LESSON1/main.c
+++ CODES AND LECTURES/LESSON1/main.c
@@ -1,11 +1,13 @@
 
 
-
+//SET MACROS FOR CUSTOM FOUND ADDRESSES, WE COULD STILL USE THE HEADER FILE BUT FOR NOW ITS TO UNDERSTAND HOW TO SEARCH THE DATASHEET
 #define SYSCTL_RCGCGPIO_R (*(( volatile unsigned long *)0x400FE608 ))
 #define GPIO_PORTF_DATA_R (*(( volatile unsigned long *)0x40025038 ))
 #define GPIO_PORTF_DIR_R  (*(( volatile unsigned long *)0x40025400 ))
 #define GPIO_PORTF_DEN_R  (*(( volatile unsigned long *)0x4002551C ))
-	
+
+
+// OTHER MACROS FOR PINS 
 #define GPIO_PORTF_CLK_EN     0x20
 #define GPIO_PORTF_PIN1_EN    0x02
 #define LED_ON1								0x02
@@ -15,6 +17,7 @@
 #define LED_ON3								0x08
 
 
+//DELAY FUNCTION 
 #define DELAY_VALUE 					1000000
 unsigned long j=0;
 void Delay(void){
@@ -40,17 +43,17 @@ int main(void){
 		
 		GPIO_PORTF_DATA_R &= LED_ON3;
 		GPIO_PORTF_DATA_R &= LED_ON2;
-		GPIO_PORTF_DATA_R |= LED_ON1;
+		GPIO_PORTF_DATA_R |= LED_ON1; //BLINK LED0N1
 		Delay();
 		
 		GPIO_PORTF_DATA_R &= LED_ON1;
 		GPIO_PORTF_DATA_R &= LED_ON2;
-		GPIO_PORTF_DATA_R |= LED_ON3;
+		GPIO_PORTF_DATA_R |= LED_ON3; //BLINK LED0N3
 		Delay();
 		
 		GPIO_PORTF_DATA_R &= LED_ON3;
 		GPIO_PORTF_DATA_R &= LED_ON1;
-		GPIO_PORTF_DATA_R |= LED_ON2;
+		GPIO_PORTF_DATA_R |= LED_ON2; / BLINK LEDON2
 		Delay();
 		
 	}
